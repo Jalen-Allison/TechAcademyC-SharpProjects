ede3eabf874edf2e3170a1aef579ec51beb4567d
Jalen Allison <97763201+Jalen-Allison@users.noreply.github.com>
2022-11-18 13:26:29 -0800

    Package Express
    Console-based application for getting a shipping quote for a package.

---
+++ Basic_C#_Programs/PackageExpress/PackageExpress/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageExpress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+
+            Console.WriteLine("Please enter package weight:");
+            int weight = Convert.ToInt32(Console.ReadLine());
+
+            if (weight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+            }
+            else if (weight <= 50)
+            {
+                Console.WriteLine("Please enter package height:");
+                int height = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please enter package length:");
+                int length = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please enter package width:");
+                int width = Convert.ToInt32(Console.ReadLine());
+
+                int total = (height + length + width);
+                int price = (height * width * length * weight / 100);
+                if (total > 50)
+                {
+                    Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                    Console.ReadLine();
+                }
+                else if (total <= 50)
+                {
+                    Console.WriteLine("Your estimated total for shipping this package is: $" + price + ".00" + "Thank You!");
+                    Console.ReadLine();
+
+
+                }
+            }
+        }
+    }
+}
