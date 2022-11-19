c3cfcb5ec7bc16f836c0630fde3ae76cb8da1431
Jalen Allison <97763201+Jalen-Allison@users.noreply.github.com>
2022-11-19 08:19:18 -0800

    Added separate while loop
    While loop the counts to 10

--- Basic_C#_Programs/WhileAndDoWhileStatements/WhileAndDoWhileStatements/Program.cs
+++ Basic_C#_Programs/WhileAndDoWhileStatements/WhileAndDoWhileStatements/Program.cs
@@ -10,6 +10,18 @@ namespace WhileAndDoWhileStatements
     {
         static void Main(string[] args)
         {
+
+            int count = 1;
+            Console.WriteLine("Printing numbers from 2 to 11");
+            while (count <= 10)   //well keep running as long as its true
+            {
+                count = count + 1; //increment by 1 after checking the condition
+                Console.WriteLine(count);
+            }
+            Console.ReadLine();
+
+
+
             Console.WriteLine("What's Tom Bradys number?");
             int guess = Convert.ToInt32(Console.ReadLine());
             bool isHunch = guess == 12;   //if guess is false keep looping
@@ -17,28 +29,29 @@ namespace WhileAndDoWhileStatements
             do        // executes the code block once, before checking if the condition is true, then it will repeat the loop as
                       // as its true
             {
-                    switch (guess)    //value of the expression is compared with the values of each case
-                                      //if there is a match, the associated block of code is executed
-                    {
-                        case 13:
-                            Console.WriteLine("Close, but wrong.");
-                            Console.WriteLine("What's Tom Bradys number?");
-                            guess = Convert.ToInt32(Console.ReadLine());
-                            break;                                        //allows program to ignore the execution of all the rest of code in the switch block if the condition is met at that point
-                        case 12:
-                            Console.WriteLine("Congrats! You guessed TB12's number.");
-                            isHunch = true;  // lets the program know that you've guessed the correct number
-                            break;
-                        default:                                           // what happens when none of other cases evaluate to true 
-                            Console.WriteLine("Nope, try again.");
-                            Console.WriteLine("What's Tom Bradys number?");
-                            guess = Convert.ToInt32(Console.ReadLine());
-                            break;
-                    }
+                switch (guess)    //value of the expression is compared with the values of each case
+                                  //if there is a match, the associated block of code is executed
+                {
+                    case 13:
+                        Console.WriteLine("Close, but wrong.");
+                        Console.WriteLine("What's Tom Bradys number?");
+                        guess = Convert.ToInt32(Console.ReadLine());
+                        break;                                        //allows program to ignore the execution of all the rest of code in the switch block if the condition is met at that point
+                    case 12:
+                        Console.WriteLine("Congrats! You guessed TB12's number.");
+                        isHunch = true;  // lets the program know that you've guessed the correct number
+                        break;
+                    default:                                           // what happens when none of other cases evaluate to true 
+                        Console.WriteLine("Nope, try again.");
+                        Console.WriteLine("What's Tom Bradys number?");
+                        guess = Convert.ToInt32(Console.ReadLine());
+                        break;
                 }
-                while (!isHunch);   // keeps looping asking you to guess until you get it correct
+            }
+            while (!isHunch);   // keeps looping asking you to guess until you get it correct
 
-                Console.Read();
+            Console.Read();
         }
+    
     }
 }
