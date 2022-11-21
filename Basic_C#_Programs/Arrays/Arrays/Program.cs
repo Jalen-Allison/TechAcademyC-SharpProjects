c5c6d9789b8cd7cee38e65169b3699b27c177109
Jalen Allison <97763201+Jalen-Allison@users.noreply.github.com>
2022-11-21 13:35:24 -0800

    Arrays/List
    Created a one-dimensional Array of strings and then one of integers. Also created a list of strings. They all ask the user to select an index of the Array and then display the integer/string.
    There is also a message that displays when the user selects an index that doesn’t exist.

---
+++ Basic_C#_Programs/Arrays/Arrays/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int[] nArray = { 21, 22, 55, 11, 88 };
+            Console.WriteLine("Select an int index of the array; (0-4) ");
+        
+
+            int index = int.Parse(Console.ReadLine());
+        if (index < 5)
+        {
+
+
+            Console.WriteLine("value is: " + nArray[index]);
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("This is not an option.");
+        }
+
+            string[] names = { "Bob", "Joey", "Arty" };
+            Console.WriteLine("Select a string index of the array; (0-2) ");
+        //int number2 = Convert.ToInt32(Console.ReadLine());
+        //bool isArray2 = number2 < 2;
+        //Console.WriteLine("That is not an option");
+
+        int nIndex = int.Parse(Console.ReadLine());
+            Console.WriteLine("value is: " + names[nIndex]);
+            Console.ReadLine();
+
+            List<string> sList = new List<string>();
+            sList.Add("Dog");
+            sList.Add("Cat");
+            Console.WriteLine("Select an String index of the list; (0-1) ");
+        //int name1 = Convert.ToInt32(Console.ReadLine());
+        //bool isArray3 = name1 < 1;
+        //Console.WriteLine("That is not an option");
+
+        int sIndex = int.Parse(Console.ReadLine());
+            Console.WriteLine("value is: " + sList[sIndex]);
+            Console.ReadLine();
+
+
+      
+
+        }
+    }
+
