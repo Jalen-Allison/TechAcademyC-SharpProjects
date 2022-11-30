b44fe607ba66a6eb078ecaaadba7c4c5a8a698fe
Jalen Allison <97763201+Jalen-Allison@users.noreply.github.com>
2022-11-29 17:32:04 -0800

    Class and Method Assignment
    Created a class with 3 methods. Asked user for input. Used the users input to do 3 different math operations and printed to screen

---
+++ Basic_C#_Programs/ClassAndMethodAssignment/ClassAndMethodAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            School student = new School(); //instaniating the class (also the object)
+            Console.WriteLine("Please enter a number");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(student.Adding(userInput)); //calling the method and passing in the user input 
+            
+
+            Console.WriteLine(student.Subtraction(userInput));
+
+            Console.WriteLine(student.Multiplying(userInput));
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Basic_C#_Programs/ClassAndMethodAssignment/ClassAndMethodAssignment/School.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethodAssignment
+{
+    public class School //the class
+    {
+        //method
+        public int Adding(int num) { 
+            return num + num;
+        }
+
+        public int Subtraction(int num)
+        {
+            return num - 2;
+        }
+
+        public int Multiplying(int num)
+        {
+            return num * num;
+        }
+    }
+}
