6e45fe5eda407d238a232451d5c8252c7b9085be
Jalen Allison <97763201+Jalen-Allison@users.noreply.github.com>
2022-11-30 08:34:33 -0800

    Main Method Assignment
    Create 3 different method under one class with the same name but have different math operations

---
+++ Basic_C#_Programs/MainMethodAssigment/MainMethodAssigment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssigment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Work employee = new Work(); //instaniating the class (also the object)
+
+            Console.WriteLine(employee.Adding(6)); //calling the method and adding 6 to it
+
+
+            Console.WriteLine(employee.Adding(4.7m)); //calling the method and substracting 4.7 to it (rounds to the nearest whole number)
+
+
+            Console.WriteLine(employee.Adding("7")); //calling the method and changing the string "7" to an int so it can do the multiplication operation  
+
+            Console.ReadLine();
+
+          
+        }
+
+    }
+}
---
+++ Basic_C#_Programs/MainMethodAssigment/MainMethodAssigment/Work.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssigment
+{
+    public class Work
+    {
+        // The three methods with same name but doing different math operations 
+        public int Adding(int num) //takes an integer
+        {
+            Console.WriteLine("This is the integer method");
+            return num + 3;
+
+        }
+
+        public int Adding(decimal num) // takes a decimal but allows for the same name and different math operation
+        {
+            int dec = Convert.ToInt32(num); //declaring "dec" and conveerting it to an int  (determines the name and data type of a variable)
+            Console.WriteLine("This is the decimal method");
+            return dec - 7;
+        }
+
+        public int Adding(string num) // takes a string but allows for the same name and different math operation
+        {
+            int str = Convert.ToInt32(num); //declaring the string and converting it to an int (determines the name and data type of a variable)
+            Console.WriteLine("This is the string method");
+            return str * 3;
+        }
+    }
+}
