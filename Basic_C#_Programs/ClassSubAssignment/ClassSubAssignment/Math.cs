5d0acb7bcf9e36b93126190f2dec4b65958a4397
Jalen Allison <97763201+Jalen-Allison@users.noreply.github.com>
2022-12-01 12:01:52 -0800

    Void method that outputs integer, overload method, output parameters
    Created a class. In that class, created a void method that outputs an integer. Had the method divide the data passed to it by 2. User enters a number. Called the method on that number. Displays the output to the screen. Divides user input by 2

---
+++ Basic_C#_Programs/ClassSubAssignment/ClassSubAssignment/Math.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubAssignment
+{
+    public class Math
+    {
+        public int Result { get; set; }
+        public void Divide(int num1)  //void method
+        {
+            Console.WriteLine("This is method 1");
+            Result = num1 / 2;
+        }
+
+        public void Divide(int num1, int num2)  //overloading the "Divide" method but having it divide by a set integer in "Program.cs"
+        {
+            Console.WriteLine("This is method 2");
+            Result = num1 / num2;
+        }
+
+
+
+        //method with output parameters
+        public string StringCounter(string word, out int count) //count is what is being outputted
+        {
+            count = word.Length; //counts how many letters are being counted 
+            string phrase = "Your word is: " + count + " letters long";
+            return phrase; //prints phrase
+        }
+    }
+}
---
+++ Basic_C#_Programs/ClassSubAssignment/ClassSubAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math number = new Math(); //Instantiates the class
+
+            Console.WriteLine("Enter a number: ");
+            int userInput = Convert.ToInt32(Console.ReadLine()); //allows user input to be converted and stored
+
+            number.Divide(userInput); // calls the method to Divide user input by 2
+            Console.WriteLine(number.Result); //displays the results
+
+            number.Divide(userInput, 2); // divides userInput by 2
+            Console.WriteLine(number.Result);
+
+            Console.WriteLine("Enter a phrase: ");
+            string input = Console.ReadLine(); //allows user input to be converted and stored
+            string sentence = number.StringCounter(input, out int count); // calls method and allows user to input then "out" throws the variable "count" which counts the length of the phrase
+            Console.WriteLine(sentence);
+
+            Console.ReadLine();
+        }
+    }
+}
