5b68ba3fda3240281fd51ea8462a29821b5a8f30
Jalen Allison <97763201+Jalen-Allison@users.noreply.github.com>
2022-11-30 16:21:40 -0800

    Void Method
    Created class with a void method that takes two integers as parameters.  Does a math operation on the first integer and display the second integer to the screen.

---
+++ Basic_C#_Programs/VoidMethod/VoidMethod/Numbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    public class Numbers
+    {
+        public void Add(int num1, int num2) //takes two integers as parameters
+        {
+            int results = num1 + 2; //does a math operation (but wont be called)
+            Console.WriteLine("The second integer is " + num2); //displays the second integer to the screen
+
+        }
+    }
+}
---
+++ Basic_C#_Programs/VoidMethod/VoidMethod/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers figure = new Numbers(); //instantiate the class/object
+
+            figure.Add(3,4); //calls method but only displays second number 
+
+            figure.Add(num1: 6, num2: 8); //calls method by parameters name but only displays second number
+
+            Console.ReadLine();
+
+        }
+    }
+}
