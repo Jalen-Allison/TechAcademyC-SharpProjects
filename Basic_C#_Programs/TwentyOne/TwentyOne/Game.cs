594d29217f7678973d7957de1d61c14dab86b101
Jalen Allison <97763201+Jalen-Allison@users.noreply.github.com>
2022-12-01 18:48:27 -0800

    Inheritance update

---
+++ Basic_C#_Programs/TwentyOne/TwentyOne/Game.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public class Game
+    {
+        public List<string> Players { get; set; }
+        public string Name { get; set; }
+        public string Dealer { get; set; }
+
+
+        public void ListPlayer()                //doesnt return anything just printing to the console
+        {
+            foreach (string player in Players)  //loops through the player property above
+            {
+                Console.WriteLine(player);
+            }
+        }
+    }
+}
--- Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
+++ Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
@@ -10,16 +10,24 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            Deck deck = new Deck(); // created object "deck"
-            deck.Shuffle(3); //Calls the Shuffle method then it shuffles the deck (amount of times requested) and returns it back to the deck 
+            TwentyOneGame game = new TwentyOneGame();                           // inherted from "Game" in the "TwentyOneGame" class
+            game.Players = new List<string>() { "Jesse", "Bill", "Joe" };       // inheriting from "Game" gives access to "Players" //{"names in curly brackets"} is instantiating with values. (value = the names)
+            game.ListPlayer();                                                   // called the "super class method method" called the method from the class inherited from
+            game.Play();                                           
+            Console.ReadLine();
+
+
+            // created object "deck"
+            //Deck deck = new Deck(); 
+            //deck.Shuffle(3);                        //Calls the Shuffle method then it shuffles the deck (amount of times requested) and returns it back to the deck 
             
 
-            foreach (Card card in deck.Cards)
-            {
-                Console.WriteLine(card.Face + " of " + card.Suit);
-            }
-            Console.WriteLine(deck.Cards.Count); //counts how many cards are made 
-            Console.ReadLine();
+            //foreach (Card card in deck.Cards)
+            //{
+            //    Console.WriteLine(card.Face + " of " + card.Suit);
+            //}
+            //Console.WriteLine(deck.Cards.Count);    //counts how many cards are made 
+            //Console.ReadLine();
         }
     }
 }
