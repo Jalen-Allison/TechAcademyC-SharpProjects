c5d407ed0076b8805ed02ba49c924a3479313c4e
Jalen Allison <97763201+Jalen-Allison@users.noreply.github.com>
2022-12-01 19:54:26 -0800

    Inheritance and Super class Assignment

---
+++ Basic_C#_Programs/Inheritance/Inheritance/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Person
+    {
+        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+
+        public void SayName() // void = doesnt return anything
+        {
+           
+            {
+                Console.WriteLine("Name:" + this.FirstName + " " + this.LastName); // will print this sentence to the console once "SayName()" is called
+            }
+        }
+    }
+
+    
+}
---
+++ Basic_C#_Programs/Inheritance/Inheritance/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee label = new Employee() { FirstName = "Sample", LastName = "Student"}; //  instantiates and initializes an Employee object
+            label.SayName();                // Calls the superclass method inside of the superclass "Person" to print the sentence to console
+            Console.ReadLine();
+            
+        }
+    }
+}
