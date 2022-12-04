9c0f56d5345df6297f4430f9286927e0edcaddb8
Jalen Allison <97763201+Jalen-Allison@users.noreply.github.com>
2022-12-04 13:42:15 -0800

    Override Operators
    Not finished

---
+++ Basic_C#_Programs/Operators/Operators/Employee.cs
@@ -0,0 +1,54 @@
+﻿namespace Operators
+{
+    class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public static bool operator ==(Employee FirstName, Employee LastName)
+
+            {
+            if (FirstName.Equals(LastName))
+                return true;
+            else
+                return false;
+            
+            }
+
+        public static bool operator !=(Employee FirstName, Employee LastName)
+
+        {
+            if (FirstName.Equals(LastName))
+                return true;
+            else
+                return false;
+        }
+
+
+        //public static Employee operator ==(Employee FirstName, Employee LastName)
+
+        //{
+        //    return FirstName == LastName;
+
+        //}
+
+        //public static Employee operator !=(Employee FirstName, Employee LastName)
+
+        //{
+        //    return FirstName != LastName;
+        //}
+
+
+
+
+
+
+        //    public static Employee operator !=(Employee employee1, Employee employee2)
+        //    {
+        //        return employee1 != employee2;
+        //    }
+        //}
+
+
+
+    }
---
+++ Basic_C#_Programs/Operators/Operators/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee employee1 = new Employee() { FirstName = "John", LastName = "Doe" };
+            //Employee employee2 = new Employee() { FirstName = "Jane", LastName = "Doh" };
+
+
+            FirstName == LastName;
+            FirstName != LastName;
+
+            Employee == FirstName;
+            Employee != LastName;
+            
+
+            Console.ReadLine();
+
+        }
+    }
+}
