e82ece48650d55bb31034fe6a38a45e57a817d9d
Jalen Allison <97763201+Jalen-Allison@users.noreply.github.com>
2022-12-04 06:13:08 -0800

    Added an Interface

--- Basic_C#_Programs/AbstractClass/AbstractClass/Employee.cs
+++ Basic_C#_Programs/AbstractClass/AbstractClass/Employee.cs
@@ -6,13 +6,16 @@ using System.Threading.Tasks;
 
 namespace AbstractClass
 {
-    class Employee : Person  //inheriting from Person class
+    class Employee : Person, IQuittable  //inheriting from Person class  //IQuittable is the interface, allows multiple inheritances
     {
         public override void SayName()    //have to override to implement 
         {
             Console.WriteLine(this.firstName + " " + this.lastName);   // will print this sentence to the console once "SayName()" is called |||  
                                                                        //.this refers to the current class instance/refers to the object through which its containing method or property was invoked
         }
-
+        public void Quit()
+        {
+            Console.WriteLine("Hit enter to quit"); //implementing the interface
+        }
     }
 }
--- Basic_C#_Programs/AbstractClass/AbstractClass/Program.cs
+++ Basic_C#_Programs/AbstractClass/AbstractClass/Program.cs
@@ -11,8 +11,11 @@ namespace AbstractClass
         static void Main(string[] args)
         {
             Employee employee = new Employee() { firstName = "Sample", lastName = "Student" };
-            employee.SayName();  //Call the SayName() method on the object. 
+            employee.SayName();  //Calls the SayName() method on the object. 
+            employee.Quit();
             Console.ReadLine();
+
+            
         }
     }
 }
