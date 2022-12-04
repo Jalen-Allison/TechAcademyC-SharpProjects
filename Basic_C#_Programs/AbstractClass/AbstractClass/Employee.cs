a58dc2329f3ab6dd06ce427b1af37d629c43c27f
Jalen Allison <97763201+Jalen-Allison@users.noreply.github.com>
2022-12-03 22:04:14 -0800

    Abstract Class

---
+++ Basic_C#_Programs/AbstractClass/AbstractClass/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    class Employee : Person  //inheriting from Person class
+    {
+        public override void SayName()    //have to override to implement 
+        {
+            Console.WriteLine(this.firstName + " " + this.lastName);   // will print this sentence to the console once "SayName()" is called |||  
+                                                                       //.this refers to the current class instance/refers to the object through which its containing method or property was invoked
+        }
+
+    }
+}
---
+++ Basic_C#_Programs/AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee() { firstName = "Sample", lastName = "Student" };
+            employee.SayName();  //Call the SayName() method on the object. 
+            Console.ReadLine();
+        }
+    }
+}
