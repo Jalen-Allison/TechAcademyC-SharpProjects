5c30cec035bb723c19832f17d14ae3e52be7dd4e
Jalen Allison <97763201+Jalen-Allison@users.noreply.github.com>
2022-12-06 05:48:54 -0800

    Added overload operators

--- Basic_C#_Programs/Operators/Operators/Employee.cs
+++ Basic_C#_Programs/Operators/Operators/Employee.cs
@@ -2,53 +2,30 @@
 {
     class Employee
     {
-        public string FirstName { get; set; }
+        //properties
+        public string FirstName { get; set; } 
         public string LastName { get; set; }
+        public int Id { get; set; }
 
-        public static bool operator ==(Employee FirstName, Employee LastName)
 
-            {
-            if (FirstName.Equals(LastName))
-                return true;
-            else
-                return false;
-            
-            }
-
-        public static bool operator !=(Employee FirstName, Employee LastName)
-
+                                                //employee1 and employee2 = parameters
+        public static bool operator ==(Employee employee1, Employee employee2)  //using bool (= the return type) so program can return true or false based off the employee Id 
+                                                                                //overloaded the "==" to allow the ability to use the "==" operator to compare the two objects once they are instantiated in the main Program
         {
-            if (FirstName.Equals(LastName))
-                return true;
-            else
-                return false;
+
+            return employee1.Id == employee2.Id;                //reutrns true or false based off the IDs being the same
+
         }
 
 
-        //public static Employee operator ==(Employee FirstName, Employee LastName)
-
-        //{
-        //    return FirstName == LastName;
-
-        //}
-
-        //public static Employee operator !=(Employee FirstName, Employee LastName)
-
-        //{
-        //    return FirstName != LastName;
-        //}
 
 
-
-
-
-
-        //    public static Employee operator !=(Employee employee1, Employee employee2)
-        //    {
-        //        return employee1 != employee2;
-        //    }
-        //}
-
-
-
+        public static bool operator !=(Employee employee1, Employee employee2)  //comparison operators must be overloaded in pairs 
+        {
+            return employee1.Id != employee2.Id;                //reutrns true or false based off the IDs being different
+        }
     }
+}
+
+
+
--- Basic_C#_Programs/Operators/Operators/Program.cs
+++ Basic_C#_Programs/Operators/Operators/Program.cs
@@ -11,16 +11,17 @@ namespace Operators
         static void Main(string[] args)
         {
 
-            Employee employee1 = new Employee() { FirstName = "John", LastName = "Doe" };
-            //Employee employee2 = new Employee() { FirstName = "Jane", LastName = "Doh" };
+            Employee employee1 = new Employee() { FirstName = "John", LastName = "Doe", Id = 1 }; //instantiating first and last name and Id
+            Employee employee2 = new Employee() { FirstName = "Jane", LastName = "Doh", Id = 2 };
 
 
-            FirstName == LastName;
-            FirstName != LastName;
+            Console.WriteLine(employee1 == employee2); //comparing the two Employee objects using the overloaded operators and will write true or false since its a bool
+            Console.WriteLine(employee1 != employee2);
 
-            Employee == FirstName;
-            Employee != LastName;
-            
+
+
+
+
 
             Console.ReadLine();
 
