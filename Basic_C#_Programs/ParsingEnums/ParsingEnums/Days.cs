68baf25c94a2b38cd18cb5c4dc0b2cf48bb1e0b2
Jalen Allison <97763201+Jalen-Allison@users.noreply.github.com>
2022-12-07 13:27:38 -0800

    Parsing Enums
    Created an enum for the days of the week.
    
    Prompted the user to enter the current day of the week.
    
    Assigned the value to a variable of that enum data type and parsed through the user input
    
    Wraped in a try/catch block and it prints "Please enter an actual day of the week.” to the console if an error occurs.

---
+++ Basic_C#_Programs/ParsingEnums/ParsingEnums/Days.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnums
+{
+    class Days
+    {
+        public DaysOfWeek DaysOfWeek { get; set; }
+    }
+    public enum DaysOfWeek
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+}
---
+++ Basic_C#_Programs/ParsingEnums/ParsingEnums/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                
+                Console.WriteLine("Enter current day of the week");
+                string userInput = Console.ReadLine();
+                DaysOfWeek days = (DaysOfWeek)Enum.Parse(typeof(DaysOfWeek),userInput); //Assigned the "DaysOfWeek" value to the variable "days" of the enum data type
+                                                                                        //and parses through DaysOfWeek to see if the user input correlates with one of
+                                                                                        //the enums
+                Console.WriteLine("The current day is " + userInput);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Please enter an actual day of the week."); 
+            }
+
+
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
