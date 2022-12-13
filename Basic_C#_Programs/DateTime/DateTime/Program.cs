1df0f7f4954fcbb0d575fa0ed5bea445f985a1b9
Jalen Allison <97763201+Jalen-Allison@users.noreply.github.com>
2022-12-13 14:09:36 -0800

    DateTime
    Prints current date and ask user for a number then adds that number to the current date and time in hours

---
+++ Basic_C#_Programs/DateTime/DateTime/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datetime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now; // Gets the current date and time
+            Console.WriteLine(now);  // Prints the current date and time to the console.
+
+
+            Console.WriteLine("Please enter a number.");
+            int uInput = Convert.ToInt32(Console.ReadLine());
+            DateTime uTime = now.AddHours(uInput); // takes the user input and adds it to the current date time hours
+
+            Console.WriteLine(uTime); // prints what time it will be from user input 
+            Console.ReadLine();
+
+
+        }
+    }
+}
