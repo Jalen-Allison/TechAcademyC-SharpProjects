19e51d7569aed48a95f2f027c897743f7af1193e
Jalen Allison <97763201+Jalen-Allison@users.noreply.github.com>
2022-12-15 14:03:19 -0800

    Constructor chain
    Created a const variable.
    
    Created a variable using the keyword “var.”
    
    Chained two constructors together.

---
+++ Basic_C#_Programs/ConstructorChain/ConstructorChain/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorChain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Employee = "Jalen";
+
+            const string jobName = "The Tech Academy";
+        }
+
+
+
+        public class NewEmployee
+        {
+            public int id { get; set; }
+            public string lastName { get; set; }
+
+
+            public NewEmployee(int id, string lastName) //1st constructor 
+            {
+                this.id = id;
+                this.lastName = lastName;
+            }
+            public NewEmployee(int id) : this(id, "")  //2nd constructor 
+            {
+            }
+            public NewEmployee(string lastName) : this(0, lastName) //chaining the two constructs together
+            {
+            }
+        }
+    }
+}
