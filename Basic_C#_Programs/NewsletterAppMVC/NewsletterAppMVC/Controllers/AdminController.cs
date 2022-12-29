f60edd128cc0b64e0e7919420f0e9742228c2f2c
Jalen Allison <97763201+Jalen-Allison@users.noreply.github.com>
2022-12-28 18:52:10 -0800

    Newsletter app

---
+++ Basic_C#_Programs/NewsletterAppMVC/NewsletterAppMVC/Controllers/AdminController.cs
@@ -0,0 +1,49 @@
+﻿using NewsletterAppMVC.Models;
+using NewsletterAppMVC.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewsletterAppMVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            using (NewsletterEntities1 db = new NewsletterEntities1())          //instiantes newletter enity object that gives access to the database
+            {
+             // var signups = db.SignUps.Where(x => x.Removed == null).ToList();                                    //lamdba syntax   //created variable signups that is equal to all the records within the SignUps database
+                var signups = (from c in db.SignUps                                                                     //link = language intergerated query
+                               where c.Removed == null
+                               select c).ToList();
+
+                var signupVMs = new List<SignupVM>();
+                foreach (var signup in signups)                                 //mapping the viewmodels from the models to the viewmodels 
+                {
+                    var signupVM = new SignupVM();
+                    signupVM.Id = signup.Id;
+                    signupVM.FirstName = signup.FirstName;
+                    signupVM.LastName = signup.LastName;
+                    signupVM.EmailAddress = signup.EmailAddress;
+                    signupVMs.Add(signupVM);
+                }
+
+                return View(signupVMs);                                         //passing list to the view
+            }
+        }
+        public ActionResult Unsubscribe(int Id)
+        {
+            using (NewsletterEntities1 db = new NewsletterEntities1())
+            {
+                var signup = db.SignUps.Find(Id);
+                signup.Removed = DateTime.Now;
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index"); //accepts string data type as a parameter
+        }
+
+    }
+}
--- Basic_C#_Programs/NewsletterAppMVC/NewsletterAppMVC/Controllers/HomeController.cs
+++ Basic_C#_Programs/NewsletterAppMVC/NewsletterAppMVC/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NewsletterAppMVC.Models;
+using NewsletterAppMVC.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
@@ -11,7 +13,7 @@ namespace NewsletterAppMVC.Controllers
     public class HomeController : Controller
     {
         //action result is used to return the models to the Views, file streams, and also redirect to the controllers 
-        public ActionResult Index()                 
+        public ActionResult Index()
         {
             return View();
         }
@@ -25,43 +27,20 @@ namespace NewsletterAppMVC.Controllers
             }
             else
             {
-                string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Newsletter;Integrated Security=True;Connect 
-                                          Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"; //Connection String  is required to connect an MVC application to a database
-
-                string queryString = @"INSERT INTO SignUps (FirstName, LastName, EmailAddress) Values
-                                        (@FirstName, @LastName, @EmailAdress)";
-
-                using (SqlConnection connection = new SqlConnection(connectionString))   //  Using Statement help prevent SQL injections so connections to databases should be wrapped in one
+                using(NewsletterEntities1 db = new NewsletterEntities1())
                 {
-                    SqlCommand command = new SqlCommand(queryString, connection);
-                    command.Parameters.Add("@FirstName", SqlDbType.VarChar);
-                    command.Parameters.Add("@LastName", SqlDbType.VarChar);
-                    command.Parameters.Add("@EmailAddress", SqlDbType.VarChar);
+                    var signup = new SignUp();
+                    signup.FirstName = firstName;
+                    signup.LastName = lastName;
+                    signup.EmailAddress = emailAddress;
 
-                    command.Parameters["@FirstName"].Value = firstName;
-                    command.Parameters["@LastName"].Value = lastName;
-                    command.Parameters["@EmailAddress"].Value = emailAddress;
+                    db.SignUps.Add(signup);
+                    db.SaveChanges();
 
-                    connection.Open();
-                    command.ExecuteNonQuery();
-                    connection.Close();
                 }
+              
                 return View("Success");
             }
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
         }
     }
 }
